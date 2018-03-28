4bdd5cfa591160cc726bac4c1997633b189ec583
master <qewrqw@dwfds.com>
2018-03-28 19:24:09 +0300

    name vasya

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
 <?php
 
+spl_autoload_register (function ($class) {
+    
+    $classFileName='vendor'.DIRECTORY_SEPARATOR.$class.'.php';
+    if(file_exists($classFileName)){
+    include_once $classFileName;
+    return true;
+    }
+    return FALSE;
+});
 
+
+
+$testObj=new Test();
+$testObj->name='Vasya';
+var_dump($testObj);
+echo ($testObj->name);
